d1dba3bc8717aa3dce6327d37aedc69f8a71f66d
SupragyaBasnet <125958893+SupragyaBasnet@users.noreply.github.com>
2024-11-23 18:08:00 +0545

    third commit

--- lib/polymorphism.dart
+++ lib/polymorphism.dart
@@ -1,1 +1,27 @@
+class Shape {
+  void draw() {
+    print('Drawing a shape.');
+  }
+}
 
+class Circle extends Shape {
+  @override
+  void draw() {
+    print('Drawing a circle.');
+  }
+}
+
+class Rectangle extends Shape {
+  @override
+  void draw() {
+    print('Drawing a rectangle.');
+  }
+}
+
+void main() {
+  Shape circle = Circle();
+  Shape rectangle = Rectangle();
+
+  circle.draw(); // Output: Drawing a circle.
+  rectangle.draw(); // Output: Drawing a rectangle.
+}
